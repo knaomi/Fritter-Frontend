97f420b683dd9d7e4eed5cc4ae930f1c5f5f985d
Naomi Kirimi <nkirimi@mit.edu>
2022-11-05 15:05:45 -0400

    added backend for like in server folder

---
+++ server/like/collection.ts
@@ -0,0 +1,151 @@
+import type {HydratedDocument, Types} from 'mongoose';
+import type {Like} from './model';
+import LikeModel from './model';
+import UserCollection from '../user/collection';
+import FreetCollection from '../freet/collection';
+
+/**
+ * This files contains a class that has the functionality to explore Likes
+ * stored in MongoDB, including adding, finding, and deleting likes.
+ *
+ */
+class LikeCollection {
+  /**
+   * Add a like to the collection
+   *
+   * @param {string} authorId - The id of the author of the like
+   * @param {string} originalFreet - The id of the originalFreet
+   * @return {Promise<HydratedDocument<Like>>} - The newly created like
+   */
+  static async addOne(authorId: Types.ObjectId | string, originalFreet: Types.ObjectId | string): Promise<HydratedDocument<Like>> {
+    const date = new Date();
+    const like = new LikeModel({
+      authorId,
+      originalFreet,
+      dateCreated: date,
+      // expiringDate: (await FreetCollection.findOne(originalFreet))?.expiringDate,
+
+    });
+    await like.save(); // Saves like to MongoDB
+    return like.populate('authorId');
+  }
+
+
+  /**
+   * Find a like by likeId
+   *
+   * @param {string} likeId - The id of the like to find
+   * @return {Promise<HydratedDocument<Like>> | Promise<null> } - The like with the given likeId, if any
+   */
+  static async findOne(likeId: Types.ObjectId | string): Promise<HydratedDocument<Like>> {
+    // return LikeModel.findOne({_id: likeId, expiringDate: {$gt: new Date()}}).populate('authorId');
+    return LikeModel.findOne({_id: likeId}).populate('authorId');
+
+  }
+
+
+/**
+   * Find a like by freetId for a given author
+   * @param {string} freetId - The id of the originalfreet
+   * @param {string} authorId - The id of the author of the like on freet to find
+   * @return {Promise<HydratedDocument<Like>> | Promise<null> } - The like on the freet by author, if any
+  */
+ static async findOneByFreetId(freetId: string, authorId: Types.ObjectId | string): Promise<HydratedDocument<Like>> {
+  const author = await UserCollection.findOneByUserId(authorId);
+  const freet = await FreetCollection.findOne(freetId);
+  return LikeModel.findOne({originalFreet: freet, authorId: author._id}).populate('authorId');
+
+
+}
+
+
+
+  /**
+   * Get all the likes in the database (i.e. on all Freets by all users)
+   *
+   * @return {Promise<HydratedDocument<Like>[]>} - An array of all of the likes
+   */
+  static async findAll(): Promise<Array<HydratedDocument<Like>>> {
+    // Retrieves likes and sorts them from most to least recent
+    // return LikeModel.find({expiringDate: {$gt: new Date()}}).sort({dateCreated: -1}).populate('authorId');
+    return LikeModel.find({}).sort({dateCreated: -1}).populate('authorId');
+
+  }
+
+  /**
+   * Get all the likes in by given author
+   *
+   * @param {string} username - The username of author of the likes
+   * @return {Promise<HydratedDocument<Like>[]>} - An array of all of the likes
+   */
+  static async findAllByUsername(username: string): Promise<Array<HydratedDocument<Like>>> {
+    const author = await UserCollection.findOneByUsername(username);
+    // return LikeModel.find({authorId: author._id, expiringDate: {$gt: new Date()}}).populate('authorId');
+    return LikeModel.find({authorId: author._id}).populate('authorId');
+
+  }
+
+  /**
+   * Get all the likes in by given author 
+   *
+   * @param {string} userId - The id of author of the likes
+   * @return {Promise<HydratedDocument<Like>[]>} - An array of all of the likes
+   */
+ static async findAllByUserId(userId: Types.ObjectId |string): Promise<Array<HydratedDocument<Like>>> {
+  const author = await UserCollection.findOneByUserId(userId);
+  // return LikeModel.find({authorId: author._id, expiringDate: {$gt: new Date()}}).populate('authorId');
+  return LikeModel.find({authorId: author._id}).populate('authorId');
+
+}
+
+  /**
+   * Get all the Likes on a Freet
+   * 
+   * @param {string} freetId - The Id of the Freet
+   * @returns Promise<Array<HydratedDocument<Like>>> - An array of all the Likes
+   */
+  static async findAllbyFreetId(freetId: Types.ObjectId | string): Promise<Array<HydratedDocument<Like>>>{
+    const freet = await FreetCollection.findOne(freetId);
+    return LikeModel.find({originalFreet: freet._id}).populate('_id');
+  }
+
+  /**
+   * Delete a like with givenlikeId.
+   *
+   * @param {string} likeId - The likeId of like to delete
+   * @return {Promise<Boolean>} - true if the like has been deleted, false otherwise
+   */
+  static async deleteOne(likeId: Types.ObjectId | string): Promise<boolean> {
+    const like = await LikeModel.deleteOne({_id: likeId});
+    return like !== null;
+  }
+
+  /**
+   * Delete all the likes by the given author
+   *
+   * @param {string} authorId - The id of author of likes
+   */
+  static async deleteMany(authorId: Types.ObjectId | string): Promise<void> {
+    await LikeModel.deleteMany({authorId});
+  }
+
+  /**
+   * Delete all the likes on a freet
+   * @param {string} freetId - The id of the Freet whose likes are being deleted
+   */
+  static async deleteManybyFreetId(freetId: Types.ObjectId | string ):Promise<void>{
+    await LikeModel.deleteMany({freetId})
+  }
+
+ /**
+   * Delete all the likes on expired freets 
+   *
+   */
+  static async deleteManybyExpiration(): Promise<void> {
+    await LikeModel.deleteMany({expiringDate: {$lte: new Date()}});
+  }
+}
+
+
+
+export default LikeCollection;
---
+++ server/like/router.ts
@@ -0,0 +1,155 @@
+import type {NextFunction, Request, Response} from 'express';
+import express from 'express';
+import LikeCollection from './collection';
+import DownFreetCollection from '../downfreet/collection';
+import * as userValidator from '../user/middleware';
+import * as likeValidator from './middleware';
+import * as freetValidator from '../freet/middleware';
+import * as util from './util';
+
+const router = express.Router();
+
+/**
+ * Get all the likes for user signed in
+ *
+ * @name GET /api/likes
+ *
+ * @return {LikeResponse[]} - A list of all the likes sorted in descending
+ *                      order by date added
+ * @throws {403} - If the user is not logged in
+
+ */
+/**
+ * Get likes by author.
+ *
+ * @name GET /api/likes?authorId=id
+ *
+ * @return {LikeResponse[]} - An array of likes created by user with id, authorId
+ * @throws {400} - If authorId is not given
+ * @throws {404} - If no user has given authorId
+ *
+ */
+
+/**
+ * Get (number of) likes on freet.
+ *
+ * @name GET /api/likes?freetId=id
+ *
+ * @return {Array<HydratedDocument<DownFreet>>} - (Number of likes) created on freet with id, freetId
+ * @throws {400} - If freetId is not given
+ * @throws {404} - If no freet has given freetId
+ *
+ */
+
+router.get(
+  '/',
+  [
+    userValidator.isUserLoggedIn,
+  ],
+  async (req: Request, res: Response, next: NextFunction) => {
+    // Check if authorId  and freetId query parameters was supplied
+    if (req.query.author !== undefined  || req.query.freet !== undefined) { 
+      next();
+      return;
+    }
+
+    // const allLikes = await LikeCollection.findAll();
+    // const response = allLikes.map(util.constructLikeResponse);
+    // res.status(200).json(response);
+    const userId = (req.session.userId as string) ?? ''; // Will not be an empty string since its validated in isUserLoggedIn
+    const authorLikes = await LikeCollection.findAllByUserId(userId);
+    const response = authorLikes.map(util.constructLikeResponse);
+    res.status(200).json(response);
+  },
+  [
+    likeValidator.isAuthorExists,
+  ],
+  async (req: Request, res: Response, next: NextFunction) => {
+    if (req.query.freet !== undefined){
+      next();
+      return;
+    }
+    const authorLikes = await LikeCollection.findAllByUsername(req.query.author as string);
+    const response = authorLikes.map(util.constructLikeResponse);
+    res.status(200).json(response);
+  },
+  [
+    freetValidator.isFreetQueryExists,
+  ],
+  async (req: Request, res: Response) => {
+   
+    const freetLikes = await LikeCollection.findAllbyFreetId(req.query.freet as string);
+    res.status(200).json(freetLikes);
+  }
+);
+
+
+// );
+
+/**
+ * Create a new like.
+ *
+ * @name POST /api/likes
+ *
+ * @param {string} freetid - The freet that the user is Liking
+ * @return {LikeResponse} - The created like
+ * @throws {403} - If the user is not logged in
+ * @throws {400} - If the user had already added a Like on the Freet
+ * @throws{404} - If the freetid does not exist.
+ */
+router.post(
+  '/',
+  [
+    userValidator.isUserLoggedIn,
+    likeValidator.isValidFreetId,
+    likeValidator.isUserAlreadyLiking,
+  ],
+  async (req: Request, res: Response) => {
+    const userId = (req.session.userId as string) ?? ''; // Will not be an empty string since its validated in isUserLoggedIn
+    const like = await LikeCollection.addOne(userId, req.body.freetid);
+    const downFreetOnFreet = await DownFreetCollection.findOneByFreetId(req.body.freetid, userId);
+    if (downFreetOnFreet){
+      await DownFreetCollection.deleteOne(downFreetOnFreet._id);
+      res.status(201).json({
+        message: 'The previous downFreet was deleted successfully \n'+
+            'and Your like was created successfully.',
+        like: util.constructLikeResponse(like)
+      });     
+    }
+    else{
+      res.status(201).json({
+        message: 'Your like was created successfully.',
+        like: util.constructLikeResponse(like)
+      });
+    }
+  }
+);
+
+/**
+ * Delete a like
+ *
+ * @name DELETE /api/like/:id
+ *
+ * @return {string} - A success message
+ * @throws {403} - If the user is not logged in or is not the author of
+ *                 the like
+ * @throws {404} - If the likeId is not valid
+ */
+router.delete(
+  '/:likeId?',
+  [
+    userValidator.isUserLoggedIn,
+    likeValidator.isLikeExists,
+    likeValidator.isValidLikeModifier
+  ],
+  async (req: Request, res: Response) => {
+    await LikeCollection.deleteOne(req.params.likeId);
+    // await LikeCollection.deleteManybyExpiration();
+    res.status(200).json({
+      message: 'Your like was deleted successfully.'
+    });
+  }
+);
+
+
+export {router as likeRouter};
