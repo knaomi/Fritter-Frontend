5ef39b991ab4fe25a3c52602149bbaeaee5dac69
Naomi Kirimi <nkirimi@mit.edu>
2022-11-06 01:17:29 -0500

    getNestsForm is similar to GetBookMarksForm thus GetBookMarksForm was deleted to avoid redundancy

--- client/components/BookMark/GetNestsForm.vue
+++ client/components/BookMark/GetNestsForm.vue
@@ -1,1 +1,47 @@
 <!-- Gets the bookmark nests aka folders for a particular user -->
+
+<template>
+    
+</template>
+
+<script>
+export default {
+  name: 'GetNestsForm',
+  data() {
+    return {value:this.$store.state.username}
+  },
+  methods: {
+    async submit() {
+      const url = '/api/bookmarknests';
+      try {
+        const r = await fetch(url);
+        const res = await r.json();
+        if (!r.ok) {
+          throw new Error(res.error);
+        }
+
+        this.$store.commit('updateNests', res);
+      } catch (e) {
+        // NOT NEEDED SINCE THE FILTERING OPTION IS NOT AVAILABLE FOR FREET DRAFT
+        // if (this.value === this.$store.state.filter) {
+        //   // This section triggers if you filter to a user but they
+        //   // change their username when you refresh
+        //   this.$store.commit('updateFilter', null);
+        //   this.value = ''; // Clear filter to show all users' freets
+        //   this.$store.commit('refreshFreetDrafts');
+        // } else {
+        //   // Otherwise reset to previous fitler
+        //   this.value = this.$store.state.filter;
+        // }
+
+        this.$set(this.alerts, e, 'error');
+        setTimeout(() => this.$delete(this.alerts, e), 3000);
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
